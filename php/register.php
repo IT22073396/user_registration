839fbada5dc91080f6e773dd2cf23716fad786c6
Molinda Mihiranga <it22073396@my.sliit.lk>
2025-02-22 18:57:14 +0530

    commit 2

--- php/register.php
+++ php/register.php
@@ -5,3 +5,26 @@
     <title>User Registration</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
+<body>
+    <div class="container mt-5">
+        <h2>User Registration</h2>
+        <form action="php/register_process.php" method="POST">
+            <div class="mb-3">
+                <label>Name</label>
+                <input type="text" class="form-control" name="name" required>
+            </div>
+            <div class="mb-3">
+                <label>Email</label>
+                <input type="email" class="form-control" name="email" required>
+            </div>
+            <div class="mb-3">
+                <label>Password</label>
+                <input type="password" class="form-control" name="password" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Register</button>
+            <a href="user_list.php" class="btn btn-info">User List</a>
+        </form>
+    </div>
+</body>
+</html>
+</html>
