6ed19c0901b616361c3e277fa01f5a91d66446f9
Molinda Mihiranga <it22073396@my.sliit.lk>
2025-02-22 19:11:20 +0530

    user list

---
+++ php/user_list.php
@@ -0,0 +1,15 @@
+<?php
+include 'php/db_connect.php';
+?>
+<!DOCTYPE <html>
+
+<head>
+    <title>User List</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
