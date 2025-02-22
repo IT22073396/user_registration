c6ac450819c8079e6b86050dd4fc6043e6962147
Molinda Mihiranga <it22073396@my.sliit.lk>
2025-02-22 17:27:07 +0530

    initial commit

---
+++ php/register.php
@@ -0,0 +1,7 @@
+<?php include 'php/db_connect.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>User Registration</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
