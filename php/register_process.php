d9197e8efae58a4e166f72305df3d6d6385d5564
Molinda Mihiranga <it22073396@my.sliit.lk>
2025-02-22 19:07:50 +0530

    register process

---
+++ php/register_process.php
@@ -0,0 +1,20 @@
+<?php
+include 'db_connect.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration Successful! <a href='../user_list.php'>View Users</a>";
+    } else {
+        echo "Error: " . $conn->error;
+    }
+}
+
+
+$conn->close();
+
+?>
