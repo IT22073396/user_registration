81d7c32345c5ed9fca6b8bfb583f268b698d5c80
Molinda Mihiranga <it22073396@my.sliit.lk>
2025-02-22 19:58:55 +0530

    user list 2.0

--- php/user_list.php
+++ php/user_list.php
@@ -9,6 +9,32 @@ include 'php/db_connect.php';
 </head>
 
 <body>
+<div class="container mt-5">
+        <h2>User List</h2>
+        <table class="table table-bordered">
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Actions</th>
+            </tr>
+            <?php
+            $sql = "SELECT * FROM users";
+            $result = $conn->query($sql);
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>
+                        <td>{$row['id']}</td>
+                        <td>{$row['name']}</td>
+                        <td>{$row['email']}</td>
+                        <td>
+                            <a href='edit.php?id={$row['id']}' class='btn btn-warning'>Edit</a>
+                            <a href='php/delete.php?id={$row['id']}' class='btn btn-danger'>Delete</a>
+                        </td>
+                     </tr>";
+            }
+            ?>
+        </table>
+    </div>
 
 </body>
 
